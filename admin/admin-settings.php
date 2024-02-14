7f3382a51669d546e92ae388d8d07c816f744d7e
eduhud <eduardomartinezcasanova@gmail.com>
2024-02-14 20:08:00 +0100

    Settings & Rules & Customization

--- admin/admin-settings.php
+++ admin/admin-settings.php
@@ -1,6 +1,7 @@
 <?php
 
-function midrocket_chatbot_gpt_add_admin_menu() {
+function midrocket_chatbot_gpt_add_admin_menu()
+{
     add_menu_page(
         'Chatbot GPT Settings',
         'Chatbot Settings',
@@ -11,22 +12,24 @@ function midrocket_chatbot_gpt_add_admin_menu() {
 }
 add_action('admin_menu', 'midrocket_chatbot_gpt_add_admin_menu');
 
-function midrocket_chatbot_gpt_settings_page() {
+function midrocket_chatbot_gpt_settings_page()
+{
     ?>
-    <div class="wrap">
-        <h2>Chatbot GPT Settings</h2>
-        <form action="options.php" method="post">
-            <?php
-            settings_fields('midrocket_chatbot_gpt_options_group');
-            do_settings_sections('midrocket_chatbot_gpt_settings');
-            submit_button();
-            ?>
-        </form>
-    </div>
+<div class="wrap">
+    <h2>Chatbot GPT Settings</h2>
+    <form action="options.php" method="post">
     <?php
+    settings_fields('midrocket_chatbot_gpt_options_group');
+    do_settings_sections('midrocket_chatbot_gpt_settings');
+    submit_button();
+    ?>
+    </form>
+</div>
+<?php
 }
 
-function midrocket_chatbot_gpt_settings_init() {
+function midrocket_chatbot_gpt_settings_init()
+{
     register_setting('midrocket_chatbot_gpt_options_group', 'midrocket_chatbot_gpt_options', 'midrocket_chatbot_gpt_options_validate');
 
     add_settings_section(
@@ -45,35 +48,99 @@ function midrocket_chatbot_gpt_settings_init() {
     );
 
     add_settings_field(
-        'midrocket_chatbot_gpt_initial_prompt',
-        'Initial Prompt',
-        'midrocket_chatbot_gpt_initial_prompt_render',
+        'midrocket_chatbot_gpt_company_name',
+        'Company Name',
+        'midrocket_chatbot_gpt_company_name_render',
+        'midrocket_chatbot_gpt_settings',
+        'midrocket_chatbot_gpt_settings_section'
+    );
+
+    if(isset($_GET['super'])) {
+        add_settings_field(
+            'midrocket_chatbot_gpt_rules_prompt',
+            'Rules Prompt',
+            'midrocket_chatbot_gpt_rules_prompt_render',
+            'midrocket_chatbot_gpt_settings',
+            'midrocket_chatbot_gpt_settings_section'
+        );
+    }
+    add_settings_field(
+        'midrocket_chatbot_gpt_specific_content',
+        'Specific Content<br><p>Paste all the raw content with the information ChatGPT should know (f.e. Full FAQ articles).</p>',
+        'midrocket_chatbot_gpt_specific_content_render',
+        'midrocket_chatbot_gpt_settings',
+        'midrocket_chatbot_gpt_settings_section'
+    );
+    add_settings_field(
+        'midrocket_chatbot_gpt_tematic_prompt',
+        'Tematic Prompt<br><p>Specify the tematic of the Chatbot, be as specific as you need to be.</p>',
+        'midrocket_chatbot_gpt_tematic_prompt_render',
         'midrocket_chatbot_gpt_settings',
         'midrocket_chatbot_gpt_settings_section'
     );
 }
 add_action('admin_init', 'midrocket_chatbot_gpt_settings_init');
 
-function midrocket_chatbot_gpt_settings_section_callback() {
+function midrocket_chatbot_gpt_settings_section_callback()
+{
     echo 'Enter your settings below:';
 }
 
-function midrocket_chatbot_gpt_api_key_render() {
+function midrocket_chatbot_gpt_api_key_render()
+{
     $options = get_option('midrocket_chatbot_gpt_options');
     ?>
-    <input type='text' name='midrocket_chatbot_gpt_options[api_key]' value='<?php echo $options['api_key']; ?>'>
+    <input type='text' name='midrocket_chatbot_gpt_options[api_key]'
+    value='<?php echo $options['api_key']; ?>'>
+<?php
+}
+
+function midrocket_chatbot_gpt_company_name_render()
+{
+    $options = get_option('midrocket_chatbot_gpt_options');
+    ?>
+    <input type='text' name='midrocket_chatbot_gpt_options[company_name]'
+    value='<?php echo $options['company_name']; ?>'>
+<?php
+}
+
+function midrocket_chatbot_gpt_rules_prompt_render()
+{
+    $options = get_option('midrocket_chatbot_gpt_options');
+    ?>
+    <textarea name='midrocket_chatbot_gpt_options[rules_prompt]' rows='5'
+    cols='50'><?php echo !empty($options['rules_prompt']) ? $options['rules_prompt'] : RULES_PROMPT; ?></textarea>
     <?php
 }
 
-function midrocket_chatbot_gpt_initial_prompt_render() {
+function midrocket_chatbot_gpt_specific_content_render()
+{
     $options = get_option('midrocket_chatbot_gpt_options');
     ?>
-    <textarea name='midrocket_chatbot_gpt_options[initial_prompt]' rows='5' cols='50'><?php echo $options['initial_prompt']; ?></textarea>
+    <textarea name='midrocket_chatbot_gpt_options[specific_content]' rows='5'
+    cols='50' placeholder="Paste here all the content the chatbot should know to give answers"><?php echo $options['specific_content']; ?></textarea>
     <?php
 }
 
-function midrocket_chatbot_gpt_options_validate($input) {
+function midrocket_chatbot_gpt_tematic_prompt_render()
+{
+    $options = get_option('midrocket_chatbot_gpt_options');
+    ?>
+<textarea name='midrocket_chatbot_gpt_options[tematic_prompt]' rows='5'
+    cols='50'><?php echo !empty($options['tematic_prompt']) ? $options['tematic_prompt'] : '(Ejemplo actual) '.TEMATIC_PROMPT; ?></textarea>
+<?php
+}
+
+function midrocket_chatbot_gpt_options_validate($input)
+{
     $new_input['api_key'] = sanitize_text_field($input['api_key']);
-    $new_input['initial_prompt'] = sanitize_textarea_field($input['initial_prompt']);
+    $new_input['company_name'] = sanitize_text_field($input['company_name']);
+    $new_input['specific_content'] = sanitize_textarea_field($input['specific_content']);
+    $new_input['tematic_prompt'] = sanitize_textarea_field($input['tematic_prompt']);
+    
+    if(isset($_GET['super'])) {
+        $new_input['rules_prompt'] = sanitize_textarea_field($input['rules_prompt']);
+    }
     return $new_input;
 }
+?>
